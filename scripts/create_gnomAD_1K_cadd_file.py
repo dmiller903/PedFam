55ad81878607f6fd1e8b82c7114a49fc5073315e
Dustin Miller <dmiller903@gmail.com>
2021-03-08 11:16:00 -0700

    allow output specification

--- scripts/create_gnomAD_1K_cadd_file.py
+++ scripts/create_gnomAD_1K_cadd_file.py
@@ -1,13 +1,28 @@
 import gzip
 import time
+import os
+import concurrent.futures
+import argparse
+import re
 
 #Keep track of when the script began
 startTime = time.time()
 char = '\n' + ('*' * 70) + '\n'
 
+# Argparse Information
+parser = argparse.ArgumentParser(description="Creates a file that has clinVar, gnomAD AF, 1K AF, and CADD\
+    values for all variant positions.")
+
+parser.add_argument('output_file', help='Name of output file')
+
+args = parser.parse_args()
+
+#Create variables of each argument from argparse
+outputFile = args.output_file
+
 def create_cadd_dict(currentChr):
     tempDict = {}
-    with gzip.open(f"cadd/cadd_chr{currentChr}.tsv.gz", "rt") as caddFile:
+    with gzip.open(f"/tmp/cadd_chr{currentChr}.tsv.gz", "rt") as caddFile:
         for line in caddFile:
             if line.startswith("#Chrom"):
                 lineList = line.rstrip("\n").split("\t")
@@ -29,7 +44,7 @@ def create_cadd_dict(currentChr):
 
 def create_clinVar_dict(currentChr):
     tempDict = {}
-    with gzip.open(f"clinVar/clinVar_chr{currentChr}.tsv.gz", "rt") as clinVarFile:
+    with gzip.open(f"/tmp/clinVar_chr{currentChr}.tsv.gz", "rt") as clinVarFile:
         for line in clinVarFile:
             if line.startswith("rsID"):
                 lineList = line.rstrip("\n").split("\t")
@@ -47,7 +62,7 @@ def create_clinVar_dict(currentChr):
 
 def create_maf_dict(currentChr):
     tempDict = {}
-    fileName = f"maf/1K_chr{currentChr}.tsv.gz"
+    fileName = f"/tmp/1K_chr{currentChr}.tsv.gz"
     with gzip.open(fileName, "rt") as mafFile:
         for line in mafFile:
             if line.startswith("#CHROM"):
@@ -76,6 +91,145 @@ def get_value_from_dict(aDict, pos, ref, alt):
                 return(aValue)
     return("None")
 
+def parse_cadd(file):
+    currentChrom = re.findall(r"_chr(\d+)\.", file)[0]
+    with gzip.open("/tmp/whole_genome_SNVs.tsv.gz", 'rt') as cadd, gzip.open(file, "wb") as output:
+        meta = cadd.readline()
+        header = cadd.readline()
+        headerList = header.rstrip("\n").split("\t")
+        chromIndex = headerList.index("#Chrom")
+        output.write(header.encode())
+        for line in cadd:
+            lineList = line.rstrip("\n").split("\t")
+            chrom = lineList[chromIndex]
+            if chrom == currentChrom:
+                output.write(line.encode())
+
+def parse_clinVar(file):
+    currentChrom = re.findall(r"_chr(\d+)\.", file)[0]
+    with gzip.open("/tmp/variant_summary.txt.gz", 'rt') as clinVar, gzip.open(file, "wb") as output:
+        header = clinVar.readline()
+        headerList = header.rstrip("\n").split("\t")
+        chromIndex = headerList.index("Chromosome")
+        rsIndex = headerList.index("RS# (dbSNP)")
+        sigIndex = headerList.index("ClinicalSignificance")
+        output.write(f"rsID\tclinicalSig\n".encode())
+        for line in clinVar:
+            lineList = line.rstrip("\n").split("\t")
+            chrom = lineList[chromIndex]
+            if chrom == currentChrom:
+                if lineList[rsIndex] != "-":
+                    rs = "rs" + lineList[rsIndex]
+                    sig = lineList[sigIndex]
+                    output.write(f"{rs}\t{sig}\n".encode())
+
+def getFiles(file):
+        os.system(f"wget --no-check-certificate {file}")
+
+def parse_1K(file):
+    currentChrom = re.findall(r"_chr(\d+)\.", file)[0]
+    with gzip.open(f"/tmp/ALL.chr{currentChrom}_GRCh38_sites.20170504.vcf.gz", 'rt') as mafFile, gzip.open(file, "wb") as output:
+        for line in mafFile:
+            if line.startswith("##"):
+                continue
+            elif line.startswith("#CHROM"):
+                lineList = line.rstrip("\n").split("\t")
+                chromIndex = lineList.index("#CHROM")
+                posIndex = lineList.index("POS")
+                refIndex = lineList.index("REF")
+                altIndex = lineList.index("ALT")
+                infoIndex = lineList.index("INFO")
+                rsIndex = lineList.index("ID")
+                output.write(f"#CHROM\tPOS\tREF\tALT\tAF\n".encode())
+            else:
+                lineList = line.rstrip("\n").split("\t")
+                chrom = lineList[chromIndex]
+                if currentChrom == chrom:
+                    pos = lineList[posIndex]
+                    ref = lineList[refIndex]
+                    alt = lineList[altIndex]
+                    info = lineList[infoIndex]
+                    rs = lineList[rsIndex]
+                    rs = rs.split(";")
+                    alt = alt.split(",")
+                    infoList = info.split(";")
+                    maf = infoList[1].replace("AF=", "")
+                    maf = maf.split(",")
+                    for j, value in enumerate(alt):
+                        output.write(f"{chrom}\t{pos}\t{ref}\t{value}\t{maf[j]}\n".encode())
+
+def parse_gnomAD(file):
+    currentChrom = re.findall(r"_chr(\d+)_", file)[0]
+    with gzip.open("/tmp/gnomad.genomes.r3.0.sites.vcf.bgz", 'rt') as gnomAD, gzip.open(file, "wb") as output:
+        output.write(f"#CHROM\tPOS\tREF\tALT\trsID\tAF\n".encode())
+        for line in gnomAD:
+            if not line.startswith("#"):
+                lineList = line.rstrip("\n").split("\t")
+                chrom = lineList[0].lstrip("chr")
+                if chrom == currentChrom:
+                    pos = lineList[1]
+                    rsID = lineList[2]
+                    ref = lineList[3]
+                    alt = lineList[4]
+                    # info is the last item in the list and the elements of info are separated by :
+                    info = lineList[-1]
+                    infoList = info.split(";")
+                    af = infoList[2]
+                    #remove "AF=" from af and convert to float instead of scientific notation.
+                    if "AF=" in af:
+                        af = float(af[3:])
+                        af = f"{af:.10f}"
+                        output.write(f"{chrom}\t{pos}\t{ref}\t{alt}\t{rsID}\t{af}\n".encode())
+
+# Download the CADD file and separate it by chromosome
+if not os.path.exists("/tmp/whole_genome_SNVs.tsv.gz"):
+    os.system("wget --no-check-certificate https://krishna.gs.washington.edu/download/CADD/v1.6/GRCh38/whole_genome_SNVs.tsv.gz -P /tmp")
+
+fileList = []
+for i in range(1, 23):
+    fileList.append(f"/tmp/cadd_chr{i}.tsv.gz")
+
+with concurrent.futures.ProcessPoolExecutor(max_workers=22) as executor:
+    executor.map(parse_cadd, fileList)
+
+# Download the clinVar file and separate by chromosome
+if not os.path.exists("/tmp/variant_summary.txt.gz"):
+    os.system("wget --no-check-certificate https://ftp.ncbi.nlm.nih.gov/pub/clinvar/tab_delimited/variant_summary.txt.gz -P /tmp")
+
+fileList = []
+for i in range(1, 23):
+    fileList.append(f"/tmp/clinVar_chr{i}.tsv.gz")
+
+with concurrent.futures.ProcessPoolExecutor(max_workers=22) as executor:
+    executor.map(parse_clinVar, fileList)
+    
+# Download 1000 genome maf file and separate by chromosome
+if not os.path.exists(f"/tmp/ALL.chr1_GRCh38_sites.20170504.vcf.gz"):
+    fileList = []
+    for i in range(1, 23):
+        fileList.append(f"http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/GRCh38_positions/ALL.chr{i}_GRCh38_sites.20170504.vcf.gz -P /tmp")
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=22) as executor:
+        executor.map(getFiles, fileList)
+
+fileList = []
+for i in range(1, 23):
+    fileList.append(f"/tmp/1K_chr{i}.tsv.gz")
+with concurrent.futures.ProcessPoolExecutor(max_workers=22) as executor:
+    executor.map(parse_1K, fileList)
+
+# Download gnomAD maf file and separate by chromosome
+if not os.path.exists("/tmp/gnomad.genomes.r3.0.sites.vcf.bgz"):
+    os.system("wget --no-check-certificate https://storage.googleapis.com/gnomad-public/release/3.0/vcf/genomes/gnomad.genomes.r3.0.sites.vcf.bgz -P /tmp")
+
+fileList = []
+for i in range(1, 23):
+    fileList.append(f"/tmp/gnomAD_chr{i}_AF.tsv.gz")
+                        
+with concurrent.futures.ProcessPoolExecutor(max_workers=22) as executor:
+    executor.map(parse_gnomAD, fileList)
+
+# Create a dictionaries containing clinvar, maf and cadd information and out to file
 for i in range(1, 23):
     #Current integer is current chromosome
     currentChr = str(i)
@@ -104,9 +258,9 @@ for i in range(1, 23):
     print(f'{char}CADD dictionary created for chromosome {currentChr}. Time elapsed: {timeElapsedMinutes} minutes ({timeElapsedHours} hours){char}')
 
     #Iterate through gnomAD file, outputting a new file with gnomAD, CADD, 1K, and Clinvar information
-    with gzip.open(f"gnomAD/gnomAD_chr{currentChr}_AF.tsv.gz", "rt") as gnomAD:
+    with gzip.open(f"/tmp/gnomAD_chr{currentChr}_AF.tsv.gz", "rt") as gnomAD:
         if currentChr == "1":
-            with gzip.open("gnomAD_1K_cadd_GRCh38.tsv.gz", "wb") as output:
+            with gzip.open(outputFile, "wb") as output:
                 for line in gnomAD:
                     if line.startswith("#CHROM"):
                         lineList = line.rstrip("\n").split("\t")
@@ -135,7 +289,7 @@ for i in range(1, 23):
                                 sig = "None" 
                             output.write(f"{currentChr}\t{pos}\t{rs}\t{ref}\t{alt}\t{cadd}\t{sig}\t{maf}\t{af}\n".encode())
         else:
-            with gzip.open("gnomAD_1K_cadd_GRCh38.tsv.gz", "ab") as output:
+            with gzip.open(outputFile, "ab") as output:
                 for line in gnomAD:
                     if line.startswith("#CHROM"):
                         lineList = line.rstrip("\n").split("\t")
